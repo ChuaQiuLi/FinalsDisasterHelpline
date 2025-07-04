9d92a7364f879e308481b7575a4b29963b61df3d
ChuaQiuLi <qiulichua@gmail.com>
2025-07-04 16:31:54 +0800

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -366,8 +366,6 @@ router.post('/saveExpoToken', async (req, res) => {
 
   const { user_id, expoPushToken } = req.body;
 
-  console.log(expoPushToken);
-
   try {
     const updatedUser = await prisma.user.update({
       where: {
@@ -382,7 +380,6 @@ router.post('/saveExpoToken', async (req, res) => {
         
     });
 
-    console.log(expoPushToken);
     console.log('Token saved for user');
     res.status(200).json({ message: 'Saved!' });
 
